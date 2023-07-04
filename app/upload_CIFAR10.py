e1c7d87bfd8f8bc1aecf69bab9f5ae352b79dff6
ryuhei-f <fukuryu115@gmail.com>
2023-07-05 01:40:16 +0900

    fix spell mistake

--- app/upload_CIFAR10.py
+++ app/upload_CIFAR10.py
@@ -10,9 +10,9 @@ transform = T.Compose([T.ToTensor()])
 path = os.path.dirname(os.path.abspath(__file__))
 
 # 学習データ
-train_data = torchvision.datasets.CIFER10(root=f'{path}/../dataset/', train=True,download=True,transform=transform)
+train_data = torchvision.datasets.CIFAR10(root=f'{path}/../dataset/', train=True,download=True,transform=transform)
 train_loader = DataLoader(train_data,batch_size = 64)
 
 # テストデータ
-test_data = torchvision.datasets.CIFER10(root=f'{path}/../dataset/', train=False,download=True,transform=transform)
+test_data = torchvision.datasets.CIFAR10(root=f'{path}/../dataset/', train=False,download=True,transform=transform)
 test_loader = DataLoader(test_data,batch_size = 64)
