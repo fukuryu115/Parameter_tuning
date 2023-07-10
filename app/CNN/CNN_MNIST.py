f850b344e1d274bc0515196d30bb3db174078344
fukuryu115 <fukuryu115@gmail.com>
2023-07-10 19:30:33 +0900

    revise CNN_MNIST.py

--- app/CNN/CNN_MNIST.py
+++ app/CNN/CNN_MNIST.py
@@ -1,36 +1,54 @@
 import tensorflow as tf
-from tensorflow.keras import layers, models
-import os
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from datetime import datetime
 
-# datasetディレクトリのパス
-dataset_path = "dataset/"
+# データセットのディレクトリパス
+path = os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-# MNISTデータセットをロードして準備する
-(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+dataset_dir = f'{path}/../dataset/'
 
-# データの正規化
-train_images = train_images.reshape((60000, 28, 28, 1))
-test_images = test_images.reshape((10000, 28, 28, 1))
-train_images, test_images = train_images / 255.0, test_images / 255.0
+# 画像データの前処理
+datagen = ImageDataGenerator(rescale=1./255)
+
+# 訓練データの読み込みと前処理
+train_generator = datagen.flow_from_directory(
+    dataset_dir + "train/",
+    target_size=(28, 28),
+    color_mode="grayscale",
+    class_mode="categorical",
+    batch_size=32
+)
+
+# テストデータの読み込みと前処理
+test_generator = datagen.flow_from_directory(
+    dataset_dir + "test/",
+    target_size=(28, 28),
+    color_mode="grayscale",
+    class_mode="categorical",
+    batch_size=32
+)
 
 # CNNモデルの構築
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
 
 # モデルのコンパイル
 model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
+              loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-# モデルの訓練
-model.fit(train_images, train_labels, epochs=5, batch_size=64)
+# TensorBoardコールバックの設定
+log_dir = "logs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1, port=6001)
 
-# モデルの評価
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-print('Test accuracy:', test_acc)
+# モデルの訓練
+model.fit(train_generator,
+          steps_per_epoch=train_generator.n // train_generator.batch_size,
+          epochs=5,
+          validation_data=test_generator,
+          validation_steps=test_generator.n // test_generator.batch_size,
+          callbacks=[tensorboard_callback])
